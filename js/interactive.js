ef3cda420f4716530bc2060aa841b925f4ab8be3
Damian Sikora <dsikora08@gmail.com>
2019-12-01 20:10:35 +0100

    [WIP] Prace nad możliwością przeglądania dostępnych artykółów

--- js/interactive.js
+++ js/interactive.js
@@ -27,3 +27,28 @@ $(function() {
 $('.navbar-toggler').click(function() {
    $(`#navbarResponsive`).addClass(`navbar--background`);
  });
+
+
+ var slideIndex = 0;
+//  showSlides(slideIndex);
+
+
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+
+ function minusSlides(n) {
+   showSlides(slideIndex -= n);
+ }
+
+
+
+
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("articles__articleBox");
+  
+   slides[slideIndex-1].className += " element_hide"; 
+   slides[slideIndex+2].style.display = "block"; 
+
+ }
