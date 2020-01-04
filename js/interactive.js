e67996c63bc86cef5f1b5df78b9b331279e7534c
Damian Sikora <dsikora08@gmail.com>
2020-01-04 18:16:57 +0100

    [WIP] Usuniecie slidera z projektu strony

--- js/interactive.js
+++ js/interactive.js
@@ -3,61 +3,77 @@ $(function() {
    var indexMidlleShow = 1;
    var width = $(window).width(); 
 
-   function plusSlides(n) {
+   $("#art1").click(function() {
+      $("#art_11").toggle();
+    });
 
-      if((width <= 991.98))
-      {
-         showSliderResponsive(indexMidlleShow += n);
-      }else{
-         showSlider(indexMidlleShow += n);
-      }  
-   }
+    $("#art2").click(function() {
+      $("#art_21").toggle();
+    });
 
-function hideAllArticles(articles)
-{
-   for (i = 0; i < articles.length; i++) {
-   articles[i].style.display = "none";
-   }
-}
+    $("#art3").click(function() {
+      $("#art_31").toggle();
+    });
 
-function showSlider(n)
-{
-   var i;
-   var slides = document.getElementsByClassName("articles__articleBox");
-   if (n > slides.length-2) {indexMidlleShow = slides.length-2}
-   if (n < 1) {indexMidlleShow = 1}
-   hideAllArticles(slides);
-   slides[indexMidlleShow-1].style.display = "block";
-   slides[indexMidlleShow].style.display = "block";
-   slides[indexMidlleShow+1].style.display = "block";
-}
+    $("#kont_img1").click(function() {
+      $("#kont1").toggle();
+    });
+
+    $("#kont_img2").click(function() {
+      $("#kont2").toggle();
+    });
 
 
-function showSliderResponsive(n)
-{
-   var i;
-   var slides = document.getElementsByClassName("articles__articleBox");
-   if (n > slides.length) {indexMidlleShow = slides.length}
-   if (n < 1) {indexMidlleShow = 1}
-   hideAllArticles(slides);
-   slides[indexMidlleShow-1].style.display = "block";
-  
-}
-
-  
-
-   if ((width <= 991.98 )) {
-      showSliderResponsive(indexMidlleShow);
-     }else {
-      showSlider(indexMidlleShow);
-     }
    
-     $(".next").click(function() {
-      plusSlides(1);
-    });
-    $(".prev").click(function() {
-      plusSlides(-1);
-    });
+   // function plusSlides(n) {
+
+   //    if((width <= 991.98))
+   //    {
+   //       showSliderResponsive(indexMidlleShow += n);
+   //    }else{
+   //       showSlider(indexMidlleShow += n);
+   //    }  
+   // }
+
+// function hideAllArticles(articles)
+// {
+//    for (i = 0; i < articles.length; i++) {
+//    articles[i].style.display = "none";
+//    }
+// }
+
+// function showSlider(n)
+// {
+//    var i;
+//    var slides = document.getElementsByClassName("articles__articleBox");
+//    if (n > slides.length-2) {indexMidlleShow = slides.length-2}
+//    if (n < 1) {indexMidlleShow = 1}
+//    hideAllArticles(slides);
+//    slides[indexMidlleShow-1].style.display = "block";
+//    slides[indexMidlleShow].style.display = "block";
+//    slides[indexMidlleShow+1].style.display = "block";
+// }
+
+
+// function showSliderResponsive(n)
+// {
+//    var i;
+//    var slides = document.getElementsByClassName("articles__articleBox");
+//    if (n > slides.length) { indexMidlleShow = slides.length}
+//    if (n < 1) {indexMidlleShow = 1}
+//    hideAllArticles(slides);
+//    slides[indexMidlleShow-1].style.display = "block";
+  
+// }
+
+  
+   
+   //   $(".next").click(function() {
+   //    plusSlides(1);
+   //  });
+   //  $(".prev").click(function() {
+   //    plusSlides(-1);
+   //  });
 
 
    var windowSizeMoreThen =true;
@@ -67,14 +83,14 @@ function showSliderResponsive(n)
       if($(window).width()>991.98)
       {
          windowSizeMoreThen=true;
-         showSlider(indexMidlleShow);
+         // showSlider(indexMidlleShow);
          
       }else
       {
          windowSizeMoreThen = false;
-         showSliderResponsive(indexMidlleShow);
-      }
-   }));
+         // showSliderResponsive(indexMidlleShow);
+        
+      }}));
   
 $('.navbar-toggler').click(function() {
    $(`#navbarResponsive`).addClass(`navbar--background`);
